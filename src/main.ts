a336e7f54f1df9e2cd98a350491ab928681a6836
timoshimmo <tokmangwang@gmail.com>
2025-11-04 07:48:31 +0100

    update name display

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,13 @@ async function bootstrap() {
   app.enableCors();
 
   // Use a global validation pipe to enforce type-safe DTOs
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Strip away properties that do not have any decorators
-    forbidNonWhitelisted: true, // Throw an error if non-whitelisted values are provided
-    transform: true, // Automatically transform payloads to DTO instances
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip away properties that do not have any decorators
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted values are provided
+      transform: true, // Automatically transform payloads to DTO instances
+    }),
+  );
 
   await app.listen(3000);
 }
