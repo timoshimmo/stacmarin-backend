e4da22119492dfe6334bd9c374cab8d01a594b01
timoshimmo <tokmangwang@gmail.com>
2025-11-21 13:07:31 +0100

    Email Service Addition 2

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -12,7 +12,7 @@ export class EmailService {
     this.transporter = nodemailer.createTransport({
       host: this.configService.get<string>('SMTP_HOST'),
       port: this.configService.get<number>('SMTP_PORT'),
-      secure: this.configService.get<boolean>('SMTP_SECURE', false), // true for 465, false for other ports
+      secure: false, // true for 465, false for other ports
       auth: {
         user: this.configService.get<string>('SMTP_USER'),
         pass: this.configService.get<string>('SMTP_PASS'),
