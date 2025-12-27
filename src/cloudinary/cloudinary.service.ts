af996276850472e7672c46cc454b6945dfc1faa9
timoshimmo <tokmangwang@gmail.com>
2025-12-27 10:45:53 +0100

    Update file upload

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -7,7 +7,10 @@ export class CloudinaryService {
     return new Promise((resolve, reject) => {
       void v2.uploader.upload(
         file,
-        { folder: 'stacconnect/avatars' },
+        {
+          folder: 'stacconnect/avatars',
+          resource_type: 'auto',
+        },
         (error, result) => {
           // eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors
           if (error) return reject(error);
