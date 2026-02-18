80b359a88951a10bb59382094c3549714874fb7c
timoshimmo <tokmangwang@gmail.com>
2026-02-18 12:33:46 +0100

    Add docuseal backend controller endpoints

--- src/documents/documents.controller.ts
+++ src/documents/documents.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { DocumentsService } from './documents.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { GetUser } from '../common/decorators/get-user.decorator';
-import { User } from '../users/entities/user.entity';
+import { User, UserRole } from '../users/entities/user.entity';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @UseGuards(JwtAuthGuard)
 @Controller('documents')
@@ -15,11 +26,25 @@ export class DocumentsController {
     return this.documentsService.getTemplates();
   }
 
+  @Post('templates')
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.MANAGER)
+  @UseInterceptors(FileInterceptor('file'))
+  async createTemplate(@Body('name') name: string, @UploadedFile() file: any) {
+    return this.documentsService.createTemplate(name, file);
+  }
+
   @Post('sign')
   async createSignatureSubmission(
     @GetUser() user: User,
     @Body('templateId') templateId: string,
   ) {
     return this.documentsService.createSubmission(templateId, user);
+  }
+
+  @Post('upload-sign')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadAndSign(@GetUser() user: User, @UploadedFile() file: any) {
+    return this.documentsService.uploadAndSign(file, user);
   }
 }
