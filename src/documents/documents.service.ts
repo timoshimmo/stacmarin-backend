67d7c08a68be359560f935adc5b4b4d392579943
timoshimmo <tokmangwang@gmail.com>
2026-03-06 17:52:10 +0100

    Add user Id as exteral ID to create submisison

--- src/documents/documents.service.ts
+++ src/documents/documents.service.ts
@@ -613,7 +613,7 @@ export class DocumentsService {
           order: body.order || 'random',
           // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
           send_email: body.send_email || true,
-          external_id: crypto.randomUUID(),
+          external_id: user.id,
         };
       } /*else {
         // Default quick sign logic
